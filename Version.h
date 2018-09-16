03a969324caec2e1f9937135c32b447cfa5d0125
AthenaADP <hownottobeseen@gmail.com>
2018-09-16 20:25:42 +1200

    Updated version to 1.00

--- Version.h
+++ Version.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define VERSION_NO 0.25b
-#define VERSION_RC 0,2,5,1
+#define VERSION_NO 1.00b
+#define VERSION_RC 1,0,0,1
 
 #define STRINGIZE2(X) #X
 #define STRINGIZE(X) STRINGIZE2(X)
 
-#define GAMES L"MHXX"
+#define GAMES L"MHXX and MHGU"
